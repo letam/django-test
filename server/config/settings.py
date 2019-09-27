ae8452f57c6fc8a900b9a3364151bafdf14b2eab
Tam Le <tamsgit@gmail.com>
2019-09-27 16:43:30 +0700

    Rename initial project folder as config

--- server/config/settings.py
+++ server/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'happiness.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'happiness.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
